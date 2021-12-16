840186c8482f5a4dbb58c8661be3a7a000b21d6a
kylebaugh <kylebaugh1@gmail.com>
2021-12-16 11:43:44 -0700

    asdf

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path')   //this is becasue we are hosting on heroku
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join (__dirname, './public/index.html'))  //this is sending back the path to the html file
+    res.sendFile(path.join(__dirname, '/public/index.html'))  //this is sending back the path to the html file
 })                                                              //path is a pre-built set of code that comes with node that functions as an object to let us do this. similar to requiring stuff like express.
 
 const port = process.env.PORT || 4545
 
-app.listen(4545, () => console.log(`Take us to warp ${port}!`))
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
 
 
